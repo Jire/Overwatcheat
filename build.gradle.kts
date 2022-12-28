4c4c05c281d6a9123fb1d96423059056145d09c6
Jire <thomasgnappo@gmail.com>
2022-12-28 12:25:46 -0500

    Added `java.time` open module for `applicationDefaultJvmArgs`

--- build.gradle.kts
+++ build.gradle.kts
@@ -49,6 +49,7 @@ application {
     applicationDefaultJvmArgs += arrayOf(
         "-Xmx8g",
         "--enable-native-access=ALL-UNNAMED",
+        "--add-opens=java.base/java.time=ALL-UNNAMED",
         "--add-exports=java.base/sun.nio.ch=ALL-UNNAMED",
         "--add-opens=java.base/java.lang=ALL-UNNAMED",
         "--add-opens=java.base/java.lang.reflect=ALL-UNNAMED",
